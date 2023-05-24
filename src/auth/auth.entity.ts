8b9d6288ba8b92c8fe09b31f46ea1d5e3fa03b59
usmanrehmat <usman.rehmat@outlook.com>
2023-05-24 12:44:19 +0500

    fixed current issues

--- src/auth/auth.entity.ts
+++ src/auth/auth.entity.ts
@@ -1,24 +1,32 @@
-import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
-import * as bcrypt from 'bcrypt';
-import { Task } from "src/tasks/task.entity";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bcrypt = require('bcryptjs');
+import { Task } from 'src/tasks/task.entity';
 
 @Unique(['username'])
 @Entity()
-export class User extends BaseEntity{
-    @PrimaryGeneratedColumn()
-    id:number;
-    @Column()
-    username: string;
-    @Column()
-    password: string;
-    @Column()
-    salt: string;
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  username: string;
+  @Column()
+  password: string;
+  @Column()
+  salt: string;
 
-    @OneToMany(() => Task, task => task.user )
-    tasks: Task[];
+  @OneToMany(() => Task, (task) => task.user)
+  tasks: Task[];
 
-    async validatePassword(password: string): Promise<boolean> {
-        const hash = await bcrypt.hash(password, this.salt);
-        return hash === this.password;
-    }
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,47 +1,49 @@
-import { ConflictException, InternalServerErrorException } from "@nestjs/common";
-import { EntityRepository, Repository } from "typeorm";
-import { User } from "./auth.entity";
-import { AuthCredentialDto } from "./dto/signup.dto";
-import * as bcrypt from 'bcrypt';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from './auth.entity';
+import { AuthCredentialDto } from './dto/signup.dto';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bcrypt = require('bcryptjs');
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-
-    async signUp(signUpDto: AuthCredentialDto): Promise<void> {
-        const {username, password} = signUpDto;
-        const user = new User();
-        user.username = username;
-        user.salt = await this.getSalt();
-        user.password = await this.hashPassword(password, user.salt);
-        try {
-            await user.save();    
-        } catch (error) {
-            console.log(error.code);
-            if(error.code === '23505') {
-                throw new ConflictException('User already exists');
-            } else {
-                throw new InternalServerErrorException();
-                
-            }
-        }       
+  async signUp(signUpDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = signUpDto;
+    const user = new User();
+    user.username = username;
+    user.salt = await this.getSalt();
+    user.password = await this.hashPassword(password, user.salt);
+    try {
+      await user.save();
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === '23505') {
+        throw new ConflictException('User already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
     }
+  }
 
-    async signIn(signIn: AuthCredentialDto): Promise<string> {
-        const {username, password} = signIn;
-        const user = await this.findOne({username});
-        if (user) {
-            if(await user.validatePassword(password)) {
-                return user.username;
-            }
-        }
-        return null;
+  async signIn(signIn: AuthCredentialDto): Promise<string> {
+    const { username, password } = signIn;
+    const user = await this.findOne({ username });
+    if (user) {
+      if (await user.validatePassword(password)) {
+        return user.username;
+      }
     }
+    return null;
+  }
 
-    private async getSalt(): Promise<string> {
-        return bcrypt.genSalt();
-    }
+  private async getSalt(): Promise<string> {
+    return bcrypt.genSalt();
+  }
 
-    private async hashPassword(password: string, salt: string) {
-        return bcrypt.hash(password, salt);
-    }
+  private async hashPassword(password: string, salt: string) {
+    return bcrypt.hash(password, salt);
+  }
 }
